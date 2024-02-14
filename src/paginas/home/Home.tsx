769a6a6e7c5b89198821a78df235fbf596a2286f
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-02-14 11:50:32 -0300

    treinando React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import'./Home.css'
+
+function Home(){
+    return(
+    <>
+        <h1 className="titulo">Manipulando o Mundo</h1>
+        <img className="imagem" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXbViihIPCcXe3dkNu-tIISqquU8yAWgN5UxCxy3f6cw&s" alt="Scooby-doo" />
+    </>
+    )
+}
+
+export default Home;
